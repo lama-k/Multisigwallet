9feb985fa675c3f17f548b09dae7f4211d658a0b
Moïse <93778660+lama-k@users.noreply.github.com>
2021-12-23 03:35:19 +0000

    Create Ownable.sol

---
+++ Ownable.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.7;
+
+contract Ownable{
+  
+  //the creator of the contract
+  address creator;
+
+  modifier onlyCreator(){
+    require(msg.sender == creator);
+    _;
+  }
+  constructor() {
+    creator = msg.sender;
+  }
+
+
+}
